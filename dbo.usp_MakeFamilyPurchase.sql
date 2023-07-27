6e6a4e0d34580a5f62e764ca2f1e5dbfda90eaa4
GAkulagin <g.kulagin2012@yandex.ru>
2023-07-27 11:46:16 +0400

    edit code according to docs

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,13 +1,13 @@
-USE ICSIT;
+use ICSIT;
 
-GO
+go
 
-CREATE PROCEDURE usp_MakeFamilyPurchase
-	@FamilySurName VARCHAR(255)
-AS
-UPDATE dbo.Family
-SET BudgetValue -= (SELECT SUM(Value)
-					FROM dbo.Basket AS B
-					JOIN dbo.Family AS F ON F.ID = B.ID_Family
-					WHERE F.SurName = @FamilySurName)
-WHERE SurName = @FamilySurName;
+create or alter procedure usp_MakeFamilyPurchase 
+	@FamilySurName varchar(255)
+as
+	update dbo.Family
+	set BudgetValue -= (select sum(Value)
+						from dbo.Basket as b
+						inner join dbo.Family as f on f.ID = b.ID_Family
+						where f.SurName = @FamilySurName)
+	where SurName = @FamilySurName
